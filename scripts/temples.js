8a2531ebba7fbe19e4395ad9d3e9c90ff76c77c4
Your Name <your.email@example.com>
2025-08-11 20:05:18 +0100

    kkkk

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,22 +1,9 @@
-// Get the current year
-const currentYear = new Date().getFullYear();
+function toggleMenu() {
+    let nav = document.getElementById("navMenu");
+    let btn = document.getElementById(".hamburger");
+    
+    nav.classList.toggle("show"); 
+    btn.classList.toggle("active");
 
-// Insert current year into the first <p> in the footer
-const footer = document.querySelector("footer");
-const firstPara = footer.querySelector("p");
-firstPara.textContent = `© ${ currentYear } | Ironbar Charles | Nigeria`;
 
-// Get the last modified date of the document
-const lastModifiedDate = document.lastModified;
-
-// Insert last modified date into the second <p> in the footer
-const secondPara = footer.querySelectorAll("p")[1];
-secondPara.textContent = `Last Modified: ${lastModifiedDate}`;
-
-const hamButton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
-
-hamButton.addEventListener("click", () => {
-    navigation.classList.toggle("open");
-    hamButton.classList.toggle("open");
-});
+} 
